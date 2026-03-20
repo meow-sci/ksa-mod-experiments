e8a7a01c7f3a178d89d889a708429ae4ace33121
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2026-03-19 21:38:35 -0400

    initial zippo

---
+++ zippo/Patcher.cs
@@ -0,0 +1,38 @@
+using System;
+using HarmonyLib;
+using Brutal.Numerics;
+using KSA;
+
+namespace mod;
+
+[HarmonyPatch]
+internal static class Patcher
+{
+    private static Harmony? _harmony = new Harmony("zippo");
+
+    public static void Patch()
+    {
+        try
+        {
+            _harmony?.PatchAll(typeof(Patcher).Assembly);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"zippo: Error applying patches: {ex.Message}");
+        }
+    }
+
+    public static void Unload()
+    {
+        try
+        {
+            _harmony?.UnpatchAll("zippo");
+            _harmony = null;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"zippo: Error removing patches: {ex.Message}");
+        }
+    }
+
+}
