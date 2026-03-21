c5ecaf75b2715b57606d44801e6af63ff3166bad
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2026-03-21 08:00:15 -0400

    lib example setup

---
+++ example-lib-project/PingModule.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MeowSci.ExampleLib;
+
+/// <summary>
+/// Example library code.
+/// </summary>
+public sealed class ExampleLibCode
+{
+  public static void Thing()
+  {
+    Console.WriteLine("Hello from the example library!");
+  }
+}
