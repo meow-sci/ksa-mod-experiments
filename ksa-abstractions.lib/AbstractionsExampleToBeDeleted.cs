c0dcbc92d8618cddb0f0b7637f98c3169fcafdf3
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2026-03-21 08:18:02 -0400

    feat: add KSA abstractions library and integrate with existing projects

---
+++ ksa-abstractions.lib/AbstractionsExampleToBeDeleted.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MeowSci.KsaAbstractions;
+
+/// <summary>
+/// Example library code.
+/// </summary>
+public sealed class AbstractionsExampleToBeDeleted
+{
+  public static void Thing()
+  {
+    Console.WriteLine("Hello from the example library!");
+  }
+}
