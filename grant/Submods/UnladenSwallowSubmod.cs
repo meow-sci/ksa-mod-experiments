8273ec3eb5645dcd2ef192985632facf0e05cb74
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2026-03-25 21:17:25 -0400

    feat(grant): add UnladenSwallowSubmod panel

---
+++ grant/Submods/UnladenSwallowSubmod.cs
@@ -0,0 +1,72 @@
+using System;
+using Brutal.Numerics;
+using Brutal.ImGuiApi;
+using MeowSci.UnladenSwallowLib;
+using MeowSci.KsaAbstractions;
+
+namespace MeowSci.Grant.Submods;
+
+internal sealed class UnladenSwallowSubmod : IGrantSubmod
+{
+    public string Name => "Unladen Swallow";
+
+    private SwallowServer? _server;
+    private bool _serverEnabled;
+
+    public void Initialize()
+    {
+        _server = new SwallowServer();
+    }
+
+    public void Update(double dt)
+    {
+        GameThread.DrainOnGameThread();
+    }
+
+    public void RenderContent()
+    {
+        ImGui.TextColored(new float4(1.0f, 0.84f, 0.0f, 1.0f), "Unladen Swallow");
+        ImGui.SeparatorText("HTTP RPC Server");
+
+        if (ImGui.Checkbox("Enable HTTP Server##us", ref _serverEnabled))
+        {
+            if (_serverEnabled)
+            {
+                try
+                {
+                    _server!.StartAsync().GetAwaiter().GetResult();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"grant/unladen-swallow: Failed to start server: {ex.Message}");
+                    _serverEnabled = false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    _server!.StopAsync().GetAwaiter().GetResult();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"grant/unladen-swallow: Failed to stop server: {ex.Message}");
+                }
+            }
+        }
+
+        if (_server is not null && _server.IsRunning)
+            ImGui.TextColored(new float4(0.0f, 1.0f, 0.4f, 1.0f), "Server: Running on http://0.0.0.0:7887");
+        else
+            ImGui.TextDisabled("Server: Stopped");
+    }
+
+    public void Dispose()
+    {
+        if (_server is not null && _server.IsRunning)
+        {
+            try { _server.StopAsync().GetAwaiter().GetResult(); }
+            catch (Exception ex) { Console.WriteLine($"grant/unladen-swallow: Error stopping server: {ex.Message}"); }
+        }
+    }
+}
