0b0ed0e2d8b57165caf036134cb993f1d00a6a84
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2026-03-29 20:50:10 -0400

    feat(marque): add initial mod structure with menu bar integration and example library

---
+++ marque.lib/MarqueLib.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MeowSci.MarqueLib;
+
+/// <summary>
+/// Example library code.
+/// </summary>
+public sealed class MarqueLib
+{
+  public static void Thing()
+  {
+    Console.WriteLine("Hello from the example library!");
+  }
+}
