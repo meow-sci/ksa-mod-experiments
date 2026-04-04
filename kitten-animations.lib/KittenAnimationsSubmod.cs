98fd21e06637eabdd01c41e644b66fef6ac6685e
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2026-04-03 21:54:38 -0400

    refactor(kitten-animations): SubmodUI conformance and UI redesign
    - Wrap RenderContent() with SubmodUI.BeginContentArea/EndContentArea
    - Show TextDisabled feedback when avatar is null instead of silent early return
    - MMU animation buttons in 2-column SizingStretchSame table (fill-width per column)
    - Expressions: duration slider in 2-column label/widget table with DragFloat
    - Add SeparatorText('Expressions') between slider and expression buttons
    - Expression buttons in 3-column SizingStretchSame table (Angry/Awe/Happy, Sad/Scared)
    - Walking: Running and Walking buttons on same line with SameLine(0, 8)

--- kitten-animations.lib/KittenAnimationsSubmod.cs
+++ kitten-animations.lib/KittenAnimationsSubmod.cs
@@ -1,4 +1,5 @@
 using System;
+using Brutal.Numerics;
 using Brutal.ImGuiApi;
 using KSA;
 using MeowSci.KsaAbstractions;
@@ -22,73 +23,142 @@ public sealed class KittenAnimationsSubmod : ISubmod
     public void RenderContent()
     {
         var avatar = KittenAvatarAccessor.GetKittenAvatar();
-        if (null == avatar) return;
+
+        SubmodUI.BeginContentArea("##ka_content");
+
+        if (avatar == null)
+        {
+            ImGui.TextDisabled("No avatar detected in scene.");
+            SubmodUI.EndContentArea();
+            return;
+        }
 
         if (ImGui.CollapsingHeader("MMU Animations"))
         {
-            if (ImGui.Button("Idle Default"))
-                KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuIdleDefaultAnim);
-            if (ImGui.Button("Move Left"))
-                KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveLeftLoopAnim);
-            if (ImGui.Button("Move Right"))
-                KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveRightLoopAnim);
-            if (ImGui.Button("Move Forward"))
-                KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveForwardLoopAnim);
-            if (ImGui.Button("Move Backward"))
-                KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveBackwardLoopAnim);
-            if (ImGui.Button("Move Up"))
-                KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveUpLoopAnim);
-            if (ImGui.Button("Move Down"))
-                KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveDownLoopAnim);
+            var tableFlags = ImGuiTableFlags.SizingStretchSame | ImGuiTableFlags.NoPadOuterX;
+            ImGui.PushStyleVar(ImGuiStyleVar.CellPadding, new float2(6f, 4f));
+            if (ImGui.BeginTable("##ka_mmu", 2, tableFlags))
+            {
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Idle Default ##ka"))
+                    KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuIdleDefaultAnim);
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Move Left ##ka"))
+                    KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveLeftLoopAnim);
+
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Move Right ##ka"))
+                    KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveRightLoopAnim);
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Move Forward ##ka"))
+                    KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveForwardLoopAnim);
+
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Move Backward ##ka"))
+                    KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveBackwardLoopAnim);
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Move Up ##ka"))
+                    KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveUpLoopAnim);
+
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Move Down ##ka"))
+                    KittenAnimationController.PlayAvatarAnimation(avatar, avatar.Animations.MmuAnimations.MmuMoveDownLoopAnim);
+
+                ImGui.EndTable();
+            }
+            ImGui.PopStyleVar(); // CellPadding
         }
 
         if (ImGui.CollapsingHeader("Expressions"))
         {
-            var duration = _animController.ExpressionDuration;
-            ImGui.SliderFloat("Expression Duration (s)", ref duration, 1.0f, 5.0f);
-            _animController.ExpressionDuration = duration;
-            ImGui.Separator();
+            // Duration slider in a 2-column label/widget table
+            var tableFlags = ImGuiTableFlags.SizingStretchProp | ImGuiTableFlags.NoPadOuterX;
+            ImGui.PushStyleVar(ImGuiStyleVar.CellPadding, new float2(6f, 6f));
+            if (ImGui.BeginTable("##ka_expr_dur", 2, tableFlags))
+            {
+                ImGui.TableSetupColumn("##lbl", ImGuiTableColumnFlags.WidthStretch, 1f);
+                ImGui.TableSetupColumn("##widget", ImGuiTableColumnFlags.WidthStretch, 3f);
 
-            if (ImGui.Button("Angry"))
-            {
-                var anim = avatar.Expressions.Angry?[_animController.Random.Next(avatar.Expressions.Angry.Count)];
-                _animController.TriggerExpression(KittenAnimationController.ExpressionType.Angry, anim, avatar);
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn();
+                ImGui.AlignTextToFramePadding();
+                ImGui.Text("Duration (s)");
+                ImGui.TableNextColumn();
+                ImGui.SetNextItemWidth(-1);
+                var duration = _animController.ExpressionDuration;
+                if (ImGui.DragFloat("##ka_expr_dur_val", ref duration, 0.1f, 1.0f, 5.0f))
+                    _animController.ExpressionDuration = duration;
+
+                ImGui.EndTable();
             }
-            if (ImGui.Button("Awe"))
+            ImGui.PopStyleVar(); // CellPadding
+
+            ImGui.SeparatorText("Expressions");
+
+            // Expression buttons in a 3-column equal-width grid
+            var btnFlags = ImGuiTableFlags.SizingStretchSame | ImGuiTableFlags.NoPadOuterX;
+            ImGui.PushStyleVar(ImGuiStyleVar.CellPadding, new float2(6f, 4f));
+            if (ImGui.BeginTable("##ka_expr_btns", 3, btnFlags))
             {
-                var anim = avatar.Expressions.Awe?[_animController.Random.Next(avatar.Expressions.Awe.Count)];
-                _animController.TriggerExpression(KittenAnimationController.ExpressionType.Awe, anim, avatar);
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Angry ##ka"))
+                {
+                    var anim = avatar.Expressions.Angry?[_animController.Random.Next(avatar.Expressions.Angry.Count)];
+                    _animController.TriggerExpression(KittenAnimationController.ExpressionType.Angry, anim, avatar);
+                }
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Awe ##ka"))
+                {
+                    var anim = avatar.Expressions.Awe?[_animController.Random.Next(avatar.Expressions.Awe.Count)];
+                    _animController.TriggerExpression(KittenAnimationController.ExpressionType.Awe, anim, avatar);
+                }
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Happy ##ka"))
+                {
+                    var anim = avatar.Expressions.Happy?[_animController.Random.Next(avatar.Expressions.Happy.Count)];
+                    _animController.TriggerExpression(KittenAnimationController.ExpressionType.Happy, anim, avatar);
+                }
+
+                ImGui.TableNextRow();
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Sad ##ka"))
+                {
+                    var anim = avatar.Expressions.Sad?[_animController.Random.Next(avatar.Expressions.Sad.Count)];
+                    _animController.TriggerExpression(KittenAnimationController.ExpressionType.Sad, anim, avatar);
+                }
+                ImGui.TableNextColumn(); ImGui.SetNextItemWidth(-1);
+                if (ImGui.Button(" Scared ##ka"))
+                {
+                    var anim = avatar.Expressions.Scared?[_animController.Random.Next(avatar.Expressions.Scared.Count)];
+                    _animController.TriggerExpression(KittenAnimationController.ExpressionType.Scared, anim, avatar);
+                }
+
+                ImGui.EndTable();
             }
-            if (ImGui.Button("Happy"))
-            {
-                var anim = avatar.Expressions.Happy?[_animController.Random.Next(avatar.Expressions.Happy.Count)];
-                _animController.TriggerExpression(KittenAnimationController.ExpressionType.Happy, anim, avatar);
-            }
-            if (ImGui.Button("Sad"))
-            {
-                var anim = avatar.Expressions.Sad?[_animController.Random.Next(avatar.Expressions.Sad.Count)];
-                _animController.TriggerExpression(KittenAnimationController.ExpressionType.Sad, anim, avatar);
-            }
-            if (ImGui.Button("Scared"))
-            {
-                var anim = avatar.Expressions.Scared?[_animController.Random.Next(avatar.Expressions.Scared.Count)];
-                _animController.TriggerExpression(KittenAnimationController.ExpressionType.Scared, anim, avatar);
-            }
+            ImGui.PopStyleVar(); // CellPadding
         }
 
         if (ImGui.CollapsingHeader("Walking Animations"))
         {
-            if (ImGui.Button("Running"))
+            if (ImGui.Button(" Running ##ka"))
             {
                 var anim = avatar.Animations.WalkingAnimations.RunningAnim;
                 KittenAnimationController.PlayAvatarAnimation(avatar, anim);
             }
-            if (ImGui.Button("Walking"))
+            ImGui.SameLine(0, 8);
+            if (ImGui.Button(" Walking ##ka"))
             {
                 var anim = avatar.Animations.WalkingAnimations.WalkingAnim;
                 KittenAnimationController.PlayAvatarAnimation(avatar, anim);
             }
         }
+
+        SubmodUI.EndContentArea();
     }
 
     public void Dispose() { }
